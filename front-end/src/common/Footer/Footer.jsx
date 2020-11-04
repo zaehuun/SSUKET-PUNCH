e3f1090b0f74f8106661bcd8bc81511a590d672e
kjwook7522 <kjwook7522@gmail.com>
2020-11-05 01:59:08 +0900

    create common components (Nav, Footer)

---
+++ front-end/src/common/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <footer id="footer">
+      <p>&copy;2020.10.07</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ front-end/src/common/Nav/Nav.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Nav.css";
+
+function Nav() {
+  return (
+    <header id="nav">
+      <Link to="/memberlist">
+        <img src="/images/logo_test.png" alt="logo-img" />
+      </Link>
+      <div className="menu">
+        <Link to="#">인맥 찾기</Link>
+        <Link to="#">내 정보</Link>
+        <Link to="#">로그아웃</Link>
+      </div>
+    </header>
+  );
+}
+
+export default Nav;
