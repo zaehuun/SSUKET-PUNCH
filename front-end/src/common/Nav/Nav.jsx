1e07254d0e79062ebb758bbd87cc90531f3fb6fa
kjwook7522 <kjwook7522@gmail.com>
2020-11-25 15:24:54 +0900

    add logout

--- front-end/src/common/Nav/Nav.jsx
+++ front-end/src/common/Nav/Nav.jsx
@@ -1,8 +1,20 @@
 import React from "react";
+import axios from "axios";
 import { Link } from "react-router-dom";
 import "./Nav.css";
 
 function Nav() {
+  const logout = () => {
+    axios
+      .post("/logout")
+      .then(res => {
+        alert("로그아웃 되었습니다.");
+        window.location.pathname = "/";
+      })
+      .catch(error => {
+        alert("Logout Error");
+      });
+  };
   return (
     <header id="nav">
       <Link to="/memberlist">
@@ -11,7 +23,7 @@ function Nav() {
       <div className="menu">
         <Link to="#">인맥 찾기</Link>
         <Link to="#">내 정보</Link>
-        <Link to="/">로그아웃</Link>
+        <a onClick={logout}>로그아웃</a>
       </div>
     </header>
   );
