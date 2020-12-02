a86fef28c606a97f192813b6f00cdc3f823da981
kjwook7522 <kjwook7522@gmail.com>
2020-12-03 01:21:52 +0900

    manage edit data of memberedit

--- front-end/src/pages/MemberEdit/MemberEdit.jsx
+++ front-end/src/pages/MemberEdit/MemberEdit.jsx
@@ -1,9 +1,88 @@
 import React from "react";
+import { useEffect, useState } from "react";
 import { Nav, Footer } from "common";
+import axios from "axios";
 import "./MemberEdit.css";
 
 function MemberEdit({ match }) {
   const id = match.params.id;
+  const memberType = {
+    id: 0,
+    name: "",
+    interest: [],
+    career: [],
+    activity: "",
+    ability: "",
+  };
+  const [memberData, setMemberData] = useState(memberType);
+  const [inputs, setInputs] = useState({
+    career: "",
+    interest: "",
+    ability: "",
+    activity: "",
+  });
+
+  const addCareer = () => {
+    let newData = { ...memberData };
+    newData.career.push(inputs.career);
+    setMemberData(newData);
+    setInputs({
+      ...inputs,
+      career: "",
+    });
+    return;
+  };
+
+  const addInterest = () => {
+    let newData = { ...memberData };
+    newData.interest.push(inputs.interest);
+    setMemberData(newData);
+    setInputs({
+      ...inputs,
+      interest: "",
+    });
+    return;
+  };
+
+  const handleInput = e => {
+    const { value, name } = e.target;
+    setInputs({
+      ...inputs,
+      [name]: value,
+    });
+  };
+
+  const handleTextarea = e => {
+    const { value, name } = e.target;
+    setMemberData({
+      ...memberData,
+      [name]: value,
+    });
+  };
+
+  const submitEdit = () => {
+    alert("제작중입니다");
+
+    // 밑에 axios로 memberData의 값 들을 전달 예정
+    axios
+      .put(`/update/${id}`, memberData)
+      .then(res => {})
+      .catch(error => {});
+  };
+
+  useEffect(() => {
+    axios
+      .get(`/member/${id}`)
+      .then(res => {
+        res.data[0].interest = JSON.parse(res.data[0].interest);
+        res.data[0].career = JSON.parse(res.data[0].career);
+        setMemberData(res.data[0]);
+      })
+      .catch(error => {
+        console.log("member get error");
+      });
+  }, [id]);
+
   return (
     <section id="member-edit">
       <Nav />
@@ -15,7 +94,7 @@ function MemberEdit({ match }) {
             <img src="/images/profile_temp.jpg" alt="profile" />
           </div>
           <div className="profile-info">
-            <h1>홍길동</h1>
+            <h1>{memberData.name}</h1>
             <h2>학생</h2>
           </div>
         </div>
@@ -23,33 +102,37 @@ function MemberEdit({ match }) {
         <div className="personal-info">
           <h1>경력</h1>
           <div className="content-list">
-            <span>test</span>
-            <span>test</span>
-            <span>test</span>
-            <span>test</span>
+            {memberData.career.map((item, idx) => (
+              <span key={idx}>{item}</span>
+            ))}
           </div>
-          <input type="text" placeholder="추가할 경력을 입력해주세요" />
-          <button>추가</button>
+          <input type="text" name="career" placeholder="추가할 경력을 입력해주세요" value={inputs.career} onChange={handleInput} />
+          <button onClick={addCareer}>추가</button>
         </div>
 
         <div className="personal-info">
           <h1>관심분야</h1>
           <div className="content-list">
-            <span>test</span>
-            <span>test</span>
+            {memberData.interest.map((item, idx) => (
+              <span key={idx}>{item}</span>
+            ))}
           </div>
-          <input type="text" placeholder="추가할 관심분야를 입력해주세요" />
-          <button>추가</button>
+          <input type="text" name="interest" placeholder="추가할 관심분야를 입력해주세요" value={inputs.interest} onChange={handleInput} />
+          <button onClick={addInterest}>추가</button>
         </div>
 
         <div className="personal-info">
           <h1>Ability</h1>
-          <textarea name="ability" id="ability"></textarea>
+          <textarea name="ability" id="ability" value={memberData.ability} onChange={handleTextarea}></textarea>
         </div>
 
         <div className="personal-info">
           <h1>Activity</h1>
-          <textarea name="activity" id="activity"></textarea>
+          <textarea name="activity" id="activity" value={memberData.activity} onChange={handleTextarea}></textarea>
+        </div>
+
+        <div className="submit-btn-wrapper">
+          <button onClick={submitEdit}>제출</button>
         </div>
       </div>
 
