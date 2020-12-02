5e772c3b3f0c6ad9bf0b68cebd183039351779ea
kjwook7522 <kjwook7522@gmail.com>
2020-12-02 17:01:09 +0900

    annotation proxy

--- front-end/src/setupProxy.js
+++ front-end/src/setupProxy.js
@@ -2,12 +2,12 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 
 module.exports = function(app) {
-    app.use('/login',createProxyMiddleware({
-                        target: 'http://localhost:5000'
-                    })
-    );
-    app.use('/join',createProxyMiddleware({
-        target: 'http://localhost:5000'
-    })
-);
+//     app.use('/login',createProxyMiddleware({
+//                         target: 'http://localhost:5000'
+//                     })
+//     );
+//     app.use('/join',createProxyMiddleware({
+//         target: 'http://localhost:5000'
+//     })
+// );
 };
